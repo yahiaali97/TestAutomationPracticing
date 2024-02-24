1e1c5b3abc4b8a5d494ccd5fa914a90cf1595117
Yahya <yahiaaali11@gmail.com>
2024-02-24 04:02:41 +0200

    first commit

---
+++ src/test/java/TestPackage/TestClass.java
@@ -0,0 +1,41 @@
+package TestPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+import java.time.Duration;
+
+public class TestClass {
+    WebDriver driver;
+
+    @BeforeTest
+    public void IndicateOFStart() {
+        System.out.println("Started");
+    }
+
+    @Test
+    public void testYouTubeSearch() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://www.youtube.com");
+        System.out.println("The Current Title: "+driver.getTitle());
+        System.out.println("The Current URL: "+driver.getCurrentUrl());
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        WebElement searchInputYoutube = driver.findElement(By.name("search_query"));
+        Actions actions = new Actions(driver);
+        actions.click(searchInputYoutube).sendKeys("جو شو").sendKeys(Keys.ENTER).perform();
+    }
+
+    @AfterTest
+    public void endSession() {
+        System.out.println("End Session");
+        driver.quit();
+    }
+}
---
+++ src/test/java/TestPackage/TestNGDemo.java
@@ -0,0 +1,13 @@
+package TestPackage;
+
+import org.testng.annotations.Test;
+
+public class TestNGDemo {
+
+    @Test
+    public void testMethod() {
+
+        System.out.println("Hello");
+
+    }
+}
