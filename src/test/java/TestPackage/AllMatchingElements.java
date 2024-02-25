a738a75b19ecdb67dec81371649fa34409dd5879
yahia <123231119+yahiaali97@users.noreply.github.com>
2024-02-25 11:35:59 +0200

    commit

---
+++ src/test/java/TestPackage/AllMatchingElements.java
@@ -0,0 +1,22 @@
+package TestPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class AllMatchingElements {
+    WebDriver driver;
+    @Test
+    public void FindAllMatchingElements() {
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://automationexercise.com/test_cases");
+        List<WebElement> Header = driver.findElements(By.tagName("li"));
+        System.out.println(Header);
+        driver.quit();
+    }
+}
