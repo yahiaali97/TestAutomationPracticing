85422d6364bffba0f6799a01464b58b8c02701e2
Yahia Ali <123231119+yahiaali97@users.noreply.github.com>
2024-03-03 00:10:47 +0200

    50+ commit

--- src/test/java/TestPackage/HandleMultipleWindows.java
+++ src/test/java/TestPackage/HandleMultipleWindows.java
@@ -1,27 +1,48 @@
 package TestPackage;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertEquals;
 
 
 public class HandleMultipleWindows extends beforeAndAfterBase {
-    @Test
-    public void doubleClickTest() throws InterruptedException {
-        driver.get("https://the-internet.herokuapp.com/windows");
 
-
-
-        String originalWindow = driver.getWindowHandle();
-        WebElement newWindowBtn = driver.findElement(By.xpath("//a[@href=\"/windows/new\"]"));
-        newWindowBtn.click();
-
-        for (String windowHandle : driver.getWindowHandles()) {
-            if(!originalWindow.contentEquals(windowHandle)) {
-                driver.switchTo().window(windowHandle);
+    @Test(enabled = false)
+    public void testWindowUsingTitle() {
+        // Store WindowHandle of parent window
+        String currentWindowID = driver.getWindowHandle();
+        driver.findElement(By.id("visitbutton")).click();
+        for (String windowID : driver.getWindowHandles()) {
+            String title = driver.switchTo().window(windowID).getTitle();
+            if (title.equals("Visit Us")) {
+                assertEquals("Visit Us", driver.getTitle());
+                // write any code to handle elements in visit us page
+                System.out.println(driver.getTitle());
+                driver.close();
                 break;
             }
         }
+        driver.switchTo().window(currentWindowID);
+    }
+
+    @Test(enabled = true)
+    public void testWindowUsingName() throws InterruptedException {
+        driver.navigate().to("http://cookbook.seleniumacademy.com/Config.html");
+        // Store WindowHandle of parent window
+        String currentWindowID = driver.getWindowHandle();
         Thread.sleep(1000);
+
+        driver.findElement(By.id("helpbutton")).click();
+        Thread.sleep(1000);
+
+        driver.switchTo().window("HelpWindow");
+        assertEquals("Help", driver.getTitle());
+        System.out.println(driver.getTitle());
+        Thread.sleep(1000);
+        
+        // code inside Help windows
+        driver.close();
+        driver.switchTo().window(currentWindowID);
     }
 }
