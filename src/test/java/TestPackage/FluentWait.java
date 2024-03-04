90ba12e36e8f455efddf3ec27775ca322043eeb3
yahia <123231119+yahiaali97@users.noreply.github.com>
2024-03-04 16:50:42 +0200

    commit

---
+++ src/test/java/TestPackage/FluentWait.java
@@ -0,0 +1,10 @@
+package TestPackage;
+
+import org.testng.annotations.Test;
+
+public class FluentWait extends beforeAndAfterBase{
+    @Test
+    public void testFluentWait() {
+        driver.get("");
+    }
+}
