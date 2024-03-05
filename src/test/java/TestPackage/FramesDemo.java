2ead414b7744798f25fbe938ed1270ce781632a7
Yahia Ali <123231119+yahiaali97@users.noreply.github.com>
2024-03-05 21:55:03 +0200

    frames

---
+++ src/test/java/TestPackage/FramesDemo.java
@@ -0,0 +1,41 @@
+package TestPackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class FramesDemo extends beforeAndAfterBase {
+    @Test
+    public void testFramesByIDOrName() {
+        driver.get("https://cookbook.seleniumacademy.com/Frames.html");
+        driver.switchTo().frame("left");
+        WebElement msg = driver.findElement(By.tagName("p"));
+        Assert.assertEquals("This is Left Frame", msg.getText());
+        System.out.println(msg.getText());
+    }
+
+    @Test(enabled = true)
+    public void testFramesDefaultContent() {
+        driver.switchTo().defaultContent();
+    }
+
+    @Test(enabled = true)
+    public void testFramesByIDOrName2() {
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame("right");
+        WebElement msg2 = driver.findElement(By.tagName("p"));
+        Assert.assertEquals("This is Right Frame", msg2.getText());
+        System.out.println(msg2.getText());
+    }
+
+    @Test
+    public void testFramesByIndex() {
+        driver.switchTo().defaultContent();
+        driver.switchTo().frame(1);
+        WebElement msg3 = driver.findElement(By.tagName("p"));
+        Assert.assertEquals("This Frame doesn't have id or name", msg3.getText());
+        System.out.println(msg3.getText());
+    }
+}
