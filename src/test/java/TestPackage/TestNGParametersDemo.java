c59a0ccd77c7cd21adc42cf5c7df4fe1e714b9bb
yahia <123231119+yahiaali97@users.noreply.github.com>
2024-03-07 15:04:34 +0200

    modified

--- src/test/java/TestPackage/TestNGParametersDemo.java
+++ src/test/java/TestPackage/TestNGParametersDemo.java
@@ -6,7 +6,7 @@ import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 
-public class TestNGParameters {
+public class TestNGParametersDemo {
     WebDriver driver;
     @BeforeTest
     public void IndicateOFStart() {
